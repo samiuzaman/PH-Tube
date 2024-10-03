60177aad7bc305802703a3455b0c4bf2cee5feec
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-10-03 13:40:20 +0600

    Created Dynamic Video Section

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function getTimeString(time) {
+  const hours = parseInt(time / 3600);
+  const remainingsecond = parseInt(time % 3600);
+  const minute = parseInt(remainingsecond / 60);
+  const second = parseInt(remainingsecond % 60);
+  return `${hours} hours ${minute} minute ${second} second ago`;
+}
+
 // show Categories
 const loadCategories = () => {
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
@@ -17,3 +25,55 @@ const displayCategories = (data) => {
 };
 
 loadCategories();
+
+// Show Videos
+const showVideos = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((response) => response.json())
+    .then((data) => displayVideos(data.videos))
+    .catch((error) => console.log(error));
+};
+const displayVideos = (videos) => {
+  const videoContainer = document.getElementById("video-container");
+  videos.forEach((video) => {
+    const div = document.createElement("div");
+    div.classList = "card card-compact";
+    div.innerHTML = `
+    <figure class="h-52 relative">
+        <img class="h-full w-full"
+        src=${video.thumbnail}
+        alt="Shoes" />
+        ${
+          video.others.posted_date?.length === 0
+            ? ""
+            : `<span class="absolute right-2 bottom-2 bg-black text-white text-xs rounded p-1">${getTimeString(
+                video.others.posted_date
+              )}</span>`
+        }
+        
+    </figure>
+    <div class=" flex gap-3 py-4">
+        <div>
+        <img class="w-10 h-10 rounded-full object-cover" src=${
+          video.authors[0].profile_picture
+        }>
+        </div>
+        <div>
+        <h3 class="text-lg font-bold text-black mb-2">${video.title}</h3>
+        <div class="flex gap-2">
+        <p>${video.authors[0].profile_name}</p>
+        ${
+          video.authors[0].verified === true
+            ? `<img class="w-5 h-5" src="https://img.icons8.com/?size=100&id=2AuMnRFVB9b1&format=png&color=000000"/>`
+            : ""
+        }
+        </div>
+        <p class="text-sm font-normal">${video.others.views}</p>
+        </div>
+    </div>
+  </div>
+    `;
+    videoContainer.append(div);
+  });
+};
+showVideos();
